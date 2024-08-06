f9131dc452ff4d65c90939b9f4f5eb3a85e314f8
VocksTrone <jr.alvarez0316@gmail.com>
2024-08-05 19:01:37 -0600

    Creación de los Atributos y Constructor de la Clase "Products"

--- Actividad#10/Products.cs
+++ Actividad#10/Products.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Actividad_10
 {
-    public class Products
+    abstract class Products
     {
-
+        public string Name { get; set; }
+        public Products(string name)
+        {
+            Name = name;
+        }
     }
 }
